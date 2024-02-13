baed08cb1971b3a6332cdb20833173f53518f7e0
kennynku <nkuatsanakeneuoe@gmail.com>
2024-02-13 14:16:37 +0000

    language

--- worker.js
+++ worker.js
@@ -12,7 +12,7 @@ const fileQueue = new Queue('thumbnail generation');
 const userQueue = new Queue('email sending');
 
 /**
- * Generates the thumbnail of an image with a given width size.
+ * Generate the thumbnail of an image with a given width size.
  * @param {String} filePath The location of the original file.
  * @param {number} size The width of the thumbnail.
  * @returns {Promise<void>}
